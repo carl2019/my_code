71713c60dfea3ca5689f64b06de16153effda849
carl2019 <1972023738@qq.com>
2019-05-09 23:23:37 +0800

    带头双向循环链表接口

---
+++ List/List.h
@@ -0,0 +1,33 @@
+#include "common.h"
+#pragma once
+
+typedef int LTDataType;
+
+typedef struct ListNode
+{
+	LTDataType _data;
+	struct ListNode* _next;
+	struct ListNode* _prev;
+}ListNode;
+
+typedef struct List
+{
+	ListNode* _head;
+}List;
+
+void ListInit(List* plist);
+void ListDestory(List* plist);
+void ListPrint(List* plist);
+
+void ListPushBack(List* plist, LTDataType x);
+void ListPushBack(List* plist);
+void ListPushFront(List* plist, LTDataType x);
+void ListPopFront(List* plist);
+
+ListNode* ListFind(List* plist, LTDataType x);
+// 在pos的前面进行插入
+void ListInsert(ListNode* pos, LTDataType x);
+// 删除pos位置的节点
+void ListErase(ListNode* pos);
+void ListRemove(List* plist, LTDataType x);
+
